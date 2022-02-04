9b8ef0dac67dd0e9532e2e122e7f88039a6950f6
Marshall Baldwin <marshallrbaldwin@ou.edu>
2022-02-04 12:16:21 -0600

    removed valid file lengths (unneccesary) and completed dump and data paths to include aerioe

--- info_dicts.py
+++ info_dicts.py
@@ -5,13 +5,6 @@ Created on Thu Oct 14 18:56:36 2021
 @author: Marshall
 """
 
-valid_filename_info = {
-    
-    'dlVAD': {'file_length': [41, 36]},
-    'dlfp' : {'file_length': 35}
-    
-    }
-
 data_info = {
     
     'dlVAD' : {
@@ -21,17 +14,16 @@ data_info = {
     
     'dlfp'  : {
         'w'       : {'name': 'Vertical Velocity', 'datamax': 5, 'datamin': -5},
-        'ws'      : {'name': 'Horizontal Wind Speed', 'datamax': None, 'datamin': None},
-        'wd'      : {'name': 'Horizontal Wind Direction', 'datamax': None, 'datamin': None},
-        'pt'      : {'name': 'Potential Temperature', 'datamax': None, 'datamin': None},
-        't'       : {'name': 'Temperature', 'datamax': None, 'datamin': None},
-        'q'       : {'name': 'Specific Humidity', 'datamax': None, 'datamin': None},
-        'dp'      : {'name': 'Dewpoint', 'datamax': None, 'datamin': None},
-        'rh'      : {'name': 'Relative Humidity', 'datamax': None, 'datamin': None},
         'std'     : {'name': 'Standard Deviation', 'datamax': None, 'datamin': None},
         'bSc'     : {'name': 'Backscatter', 'datamax': -3, 'datamin': -8},
         'bSc_TALL': {'name': 'Backscatter -- Full Range', 'datamax': -3, 'datamin': -8},
         'snr'     : {'name': 'Intensity', 'datamax': 1.6, 'datamin': 1}
+        },
+    
+    'aeri': {
+        'ptemp'      : {'name': 'Potential Temperature', 'datamax': None, 'datamin': None},
+        'temp'       : {'name': 'Temperature', 'datamax': None, 'datamin': None},
+        'dewpt'      : {'name': 'Dewpoint', 'datamax': None, 'datamin': None},
         }
     }
 
@@ -39,8 +31,14 @@ file_paths = {
     
     'dump':{
         'C1':{
-            'dlVAD' : '/data/mbaldwin/visualizations/clampsdlvadC1.c1',
-            'dlfp'  : '/data/clamps/clamps1/ingested/clampsdlfpC1.b1'
+            'dlVAD' : ['/data/mbaldwin/visualizations/clampsdlvadC1'],
+            'dlfp'  : ['/data/mbaldwin/visualizations/clampsdlfpC1'],
+            'aerioe': ['/data/mbaldwin/visualizations/clampsaerioeC1']
+            },
+        'C2':{
+            'dlVAD' : ['/data/mbaldwin/visualizations/clampsdlvadC2'],
+            'dlfp'  : ['/data/mbaldwin/visualizations/clampsdlfpC2'],
+            'aerioe': ['/data/mbaldwin/visualizations/clampsaerioeC2']
             }
         },
     
@@ -48,7 +46,14 @@ file_paths = {
         'C1':{
             'dlVAD' : ['/data/clamps/clamps1/processed/clampsdlvad1turnC1.c1',
                        '/data/clamps/clamps1/processed/clampsdlvadC1.c1'],
-            'dlfp' : '/data/clamps/clamps1/ingested/clampsdlfpC1.b1'
-            }    
+            'dlfp'  : ['/data/clamps/clamps1/ingested/clampsdlfpC1.b1'],
+            'aerioe': ['/data/clamps/clamps1/realtime/clampsaerioe1turnC1.c1']
+            },
+        'C2':{
+            'dlVAD' : ['/data/clamps/clamps2/processed/clampsdlvad1turnC2.c1',
+                       '/data/clamps/clamps2/processed/clampsdlvadC2.c1'],
+            'dlfp'  : ['/data/clamps/clamps2/ingested/clampsdlfpC2.b1/clampsdlfpC2.b1'],
+            'aerioe': ['/data/clamps/clamps2/realtime/clampsaerioe1turnC2.c1']
+            }
         }
     }
