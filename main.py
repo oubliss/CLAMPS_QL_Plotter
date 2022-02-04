e238c697bb6a17c9b2a9ffe48ec39cf06e9f7ed3
Marshall Baldwin <marshallrbaldwin@ou.edu>
2022-02-04 12:17:23 -0600

    Added main file to run plotter -- TODO

---
+++ main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  4 11:57:16 2022
+
+@author: Marshall
+"""
+
+def main():
+    
+    #do our imports
+    import os
+    from netCDF4 import Dataset
+
+if __name__ == "__main__":
+    main()
