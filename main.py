fa2292d2f77f904134a43e89507dc42cae9ddaa1
marshallrbaldwin <marshallrbaldwin@ou.edu>
2022-03-31 18:40:02 -0500

    rearranged error log opening so that it actually records all the errors thrown

--- main.py
+++ main.py
@@ -26,22 +26,21 @@ def main():
     error_log_path = file_paths['error_log']
     
     #now that we have a list of folders, loop through list
-    for folder in data_folders:
-        
-        #get the names of the files in the folder
-        files = os.listdir(folder)
-        for file in files:
+    with open(error_log_path, 'w') as error_log: #error log will be DELETED on each new run
+        for folder in data_folders:
             
-            #check if it's a valid netCDF file
-            if Plotter.is_valid_file(file):
-                print(f"{file} accessed")
+            #get the names of the files in the folder
+            files = os.listdir(folder)
+            for file in files:
                 
-                #TEMPORARY CHECK
-                #check whether data is from the date range we want
-                #namely from 20210621 - 20210629
-                file_date = int(file[-19:-11])
-                
-                with open(error_log_path, 'w') as error_log: #error log will be DELETED on each new run
+                #check if it's a valid netCDF file
+                if Plotter.is_valid_file(file):
+                    print(f"{file} accessed")
+                    
+                    #TEMPORARY CHECK
+                    #check whether data is from the date range we want
+                    #namely from 20210621 - 20210629
+                    file_date = int(file[-19:-11])
                 
                     if file_date <= 20230629 and file_date >= 20000621: #these new dates should encompass all possible dates in the files
                         try:
